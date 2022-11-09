b3c45f13aad13e48fa4913218b83e322e5d40a77
hazdard <sachabenarous@hotmail.fr>
2022-11-09 20:58:42 +0100

    exo 1 quasi fini

--- tablist.c
+++ tablist.c
@@ -2,23 +2,31 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/* C quoi typedef (et pk ça permet de retirer les struct) ? Q6 suffit il de faire .size ? Q5 calcul taille liberee, et faut il liberer les cellules aussi ? Q8 que faire si "le" point n'est pas unique ? */
-
-typedef struct elem_s elem_t; 
-struct elem_s { int x ; int y ; bool is_free ; int next ; int prev ; } ;
-
 #define MAX_CAP 100 
 
+typedef struct elem_s elem_t; 
+struct elem_s {
+    int     x;
+    int     y;
+    bool    is_free;
+    int     next;
+    int     prev;
+};
+
 typedef struct tlist_s tlist_t; 
-struct tlist_s {elem_t tab[MAX_CAP]; int size ; int first ; int last ; } ;
+struct tlist_s {
+    elem_t  tab[MAX_CAP]; 
+    int     size ; 
+    int     first ; 
+    int     last ; 
+};
 
 
 tlist_t *tlist_new() {
     tlist_t *l = malloc(sizeof(tlist_t));
-    l->size = 0;
-    l->first = l->last = -1; // Initialisation a -1 afin de caracteriser l'absence premier ou dernier element car -1 ne peut pas arriver de manière naturelle 
-    for(int i = 1; i<=100 ; i++)
-        l->tab[i].is_free = false ;
+    (l->size) = 0;
+    for(int i = 0; i<MAX_CAP ; i++)
+        (l->tab)[i].is_free = false ;
     return l ;
 }
 
@@ -40,35 +48,40 @@ int ind_libre(tlist_t *l){
         if ((l->tab)[i].is_free==false)
             return i ;
     }
-    return -1 ;
+    return -42 ;
 }
-/* Dans tout ce qui suit, si les champs n'ont pas de sens, on attribue une valeur quelconque */
 int tlist_add(tlist_t* l, int a, int b) {
     if ((l->size) == MAX_CAP)
         return 1;
     else {
-        int newlast, prevlast ;
         if ((l->size) == 0){
-            (l->first)=0;
-            int prevlast=0 ;
-            int newlast=1;
+            (l->first)=0 ;
+            (l->last) = 0 ;
+            (l->tab)[0].x=a;
+            (l->tab)[0].y=b;
+            (l->tab)[0].is_free=true;
+            (l->tab)[0].prev=-42;
+            (l->tab)[0].next=-42;
+            ++(l->size);
         }
         else {
             int newlast = ind_libre(l);
             int prevlast=(l->last);
             (l->tab)[prevlast].next= newlast;
+            (l->last) = newlast ;
+            (l->tab)[newlast].x=a;
+            (l->tab)[newlast].y=b;
+            (l->tab)[newlast].is_free=true;
+            (l->tab)[newlast].prev=prevlast;
+            (l->tab)[newlast].next=-42;
+            ++(l->size);
         }
-        (l->last) = newlast ;
-        (l->tab)[newlast].prev=prevlast;
-        (l->tab)[newlast].x=a;
-        (l->tab)[newlast].y=b;
-        (l->tab)[newlast].is_free=true;
-        ++(l->size);
+        
         return 0;
     }
 }
 
-/* l'entrée doit  un pointeur car sinon l'ajout ne serait pas appliqué de manière efefctive lors d'un appel dans une autre fonction, on a besoin de simuler un passage par référence à l'aide d'un pointeur. */
+/* l'entrée doit  un pointeur car sinon l'ajout ne serait pas appliqué de manière effective lors d'un appel dans une autre fonction, on a besoin de simuler un passage par référence à l'aide d'un pointeur. */
 
 int tlist_remove(tlist_t *l, int x, int y){
     for(int i=0 ; i<MAX_CAP; ++i){
@@ -104,76 +117,105 @@ int tlist_top(tlist_t* l, int *x, int *y){
 }
 
 int tlist_push(tlist_t* l, int x, int y){
-    if (l->size == MAX_CAP)
-        return 0 ;
-    int *a;
-    int *b;
-    tlist_top(l, a, b);
-    tlist_pop(l);
-    tlist_add(l, x, y);
-    if (l->size > 0)
-        tlist_add(l, *a, *b);
-    return 1;
+    if ((l->size) == MAX_CAP)
+        return 1;
+    else {
+        if ((l->size) == 0){
+            (l->first)= 0 ;
+            (l->last) = 0 ;
+            (l->tab)[0].x=x;
+            (l->tab)[0].y=y;
+            (l->tab)[0].is_free=true;
+            (l->tab)[0].prev=-42;
+            (l->tab)[0].next=-42;
+            ++(l->size);
+        }
+        else {
+            int newfirst = ind_libre(l);
+            int prevfirst=(l->first);
+            (l->tab)[prevfirst].prev= newfirst;
+            (l->first) = newfirst ;
+            (l->tab)[newfirst].x=x;
+            (l->tab)[newfirst].y=y;
+            (l->tab)[newfirst].is_free=true;
+            (l->tab)[newfirst].prev=-42;
+            (l->tab)[newfirst].next=prevfirst;
+            ++(l->size);
+        }
+        
+        return 0;
+    }
+}
+
+
+int dist(elem_t a){
+    int abs=a.x;
+    int ord = a.y ;
+    return ( abs*abs + ord*ord);
+}
+
+elem_t acces(tlist_t *l, int i){
+    int indi=-1;
+    for(int k=(l->first),compt=0;indi==-1;k=(l->tab)[k].next,++compt){
+        if (compt==i) {
+            indi=k;
+            }
+    }
+    return(l->tab[indi]);
 }
 
 int tlist_swap(tlist_t* l, int i, int j) {
     if ((l->size >= i) && (l->size >= j)){
         int indi=-1;
         int indj=-1;
-        for(int k=(l->first), compt=0;(indj!=-1)&&(indi!=-1);k=(l->tab)[k].next, ++compt){
-            if (compt==i) 
+        for(int k=(l->first),compt=0 ;(indj==-1)||(indi==-1);k=(l->tab)[k].next,++compt){
+            if (compt==i) {
                 indi=k;
-            if (compt==j)
+            }
+            if (compt==j){
                 indj=k;
-        elem_t temp=(l->tab)[indi] ;
-        (l->tab)[indi]=(l->tab)[indj];
-        (l->tab)[indj]=temp;
+            }
+        } 
+        elem_t temp ;
+        temp.x=(l->tab)[indi].x;
+        temp.y=(l->tab)[indi].y;
+
+        (l->tab)[indi].x=(l->tab)[indj].x;
+        (l->tab)[indi].y=(l->tab)[indj].y;
+
+        (l->tab)[indj].x=temp.x;
+        (l->tab)[indj].y=temp.y;
         return 1 ;
+    }
     return 0;
-        }   
-    }
 }
 
-double dist(elem_t a){
-    double x=(double)(a.x) ;
-    double y=(double)(a.y) ;
-    return (x*x + y*y);
-}
-
-bool est_str_avant(tlist_t *l, int i, int j){
-    if (i==(l->last))
-            return false;
-    while (j != (l->last)){
-        j=(l->tab)[j].next;
-        if (j==i)
-            return false;
-    }
-    return true ;
-}
 
 void quick_sort(tlist_t *l, int first, int last) {
     int pivot, i, j;
-    pivot = first;
-    i = first;
-    j = last;
-    while (est_str_avant(l,i,j)) {
-        while(dist((l->tab)[i]) <= dist((l->tab)[pivot]) && (i!=(l->last)))
-            i=(l->tab)[i].next;
-        while(dist((l->tab)[j]) <= dist((l->tab)[pivot]))
-            j=(l->tab)[j].prev ;
-        if(est_str_avant(l,i,j)) {
-            tlist_swap(l,i,j);
+    if(first < last) {    
+        pivot = first;
+        i = first;
+        j = last;
+        while (i<j) {
+            while(dist(acces(l,i)) <= dist(acces(l,pivot)) && i<(l->size))
+                i++;
+            while(dist(acces(l,j)) > dist(acces(l,pivot)))
+                j-- ;
+            if(i<j){
+                tlist_swap(l,i,j);
+            }
         }
+        tlist_swap(l,pivot,j);
+        quick_sort(l, first, j-1);
+        quick_sort(l,j+1,last);
     }
-    tlist_swap(l,pivot,j);
-    quick_sort(l, first, (l->tab)[j].prev);
-    quick_sort(l, (l->tab)[j].next, last);
 }
 
 int tlist_sort(tlist_t* l){
     if (l->size==0)
         return 0;
-    quick_sort(l,(l->first),(l->last));
+    quick_sort(l,0,(l->size)-1);
     return 1;
 }
 
@@ -190,10 +232,17 @@ int tlist_print(tlist_t* l){
 
 int main() {
     tlist_t* l=tlist_new();
-    int a = tlist_add(l,2,3);
-    /*tlist_sort(l);
-    *tlist_print(l);
-    tlist_free(l);*/
+    tlist_push(l,0,1);
+    tlist_push(l,0,3);
+    tlist_push(l,0,7);
+    tlist_push(l,0,8);
+    tlist_push(l,0,2);
+    tlist_push(l,0,4);
+    tlist_push(l,0,5);
+    tlist_push(l,0,6);
+    tlist_print(l);
+    tlist_sort(l);
+    tlist_print(l);
     return 0 ;
 }
 
