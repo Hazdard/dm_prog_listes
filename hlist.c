ef4d61d23361a2d32ddd9f6e328fc6556ca30957
Hazdard <sacha.ben-arous@ens-paris-saclay.fr>
2022-11-11 21:23:11 -0600

    Partie 2 en cours

--- hlist.c
+++ hlist.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
 
 typedef struct hnode_s hnode_t; 
 struct hnode_s {
@@ -9,6 +10,7 @@ struct hnode_s {
     bool        is_pinf;
     hnode_t*    next ;
     hnode_t*    prev ;
+    hnode_t*    proj ;
 };
 
 typedef struct hlist_s hlist_t; 
@@ -53,20 +55,76 @@ int hlist_search(hlist_t *l, int v, hnode_t** path){
     hnode_t* candidat = l->head ;
     int etage = 1 ;
     while(etage<=l->height){
-        while(!((candidat->next)->is_pinf)&&((candidat->next)->value <= v)){
+        while(!((candidat->next)->is_pinf)&&((candidat->next)->value < v)){
             candidat=candidat->next ;
         }
         path[etage]=candidat ;
+        if(etage<l->height)
+            candidat=candidat->proj;
         ++etage ;
-        while(!(candidat->is_minf)){
-            candidat=candidat->next ;
-        }
     }
     return(!(candidat->is_minf)&&(candidat->value == v));
 }
 
+void hlist_add_layer(hlist_t *l){
+    hnode_t* minf = malloc(sizeof(hnode_t));
+    hnode_t* pinf = malloc(sizeof(hnode_t));
+
+    pinf->is_minf=false;
+    pinf->is_pinf=true;
+    pinf->prev=minf;
+    pinf->next=l->head;
+    hnode_t* inf_search=l->head;
+    while(!(inf_search->is_pinf))
+        inf_search=inf_search->next ;
+    pinf->proj=inf_search;
+
+    minf->is_minf=true;
+    minf->is_pinf=false;
+    minf->prev=minf;
+    minf->next=pinf;
+    minf->proj=l->head;
+
+    l->head=minf;
+    ++l->height;
+}
+
 int hlist_add(hlist_t *l, int v){
-    
+    hnode_t** path = malloc(sizeof(hnode_t* [l->height]));
+    hlist_search(l,v,path);
+    int etage = l->height;
+    srand(time(NULL));
+    bool first = true ;
+    while((rand()%2)||first){
+        hnode_t* new = malloc(sizeof(hnode_t)); 
+        new->value=v;
+        new->is_minf=false;
+        new->is_pinf=false;
+        if(etage>=1){
+            new->prev=path[etage];
+            new->next=path[etage]->next;
+            if(etage<l->height)
+                new->proj=path[etage+1]->next ;
+            path[etage]->next=new;
+            (path[etage]->next)->prev=new;
+            first=false ;
+            if(etage==1){
+                hlist_add_layer(l);
+            }
+            --etage;
+        }
+        else{
+            new->prev=l->head;
+            new->next=(l->head)->next;
+            new->proj=path[etage+1]->next;
+
+            ((l->head)->next)->prev=new;
+            (l->head)->next=new;
+            hlist_add_layer(l);
+        }
+    }
+    free(path);
+    return 0;
 }
 
 int main(){
